e0a4bb8b3e17c5ccc7fe28309b8e3f7bc4c033ca
Anurag Naik <anuragnaik02@gmail.com>
2022-11-08 00:06:30 +0530

    second commit

---
+++ second.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        nums.sort()
+        i=0
+        while i<=len(nums)-1:
+            if i==len(nums)-1:
+                return nums[i]
+            if nums[i]==nums[i+1]==nums[i+2] :
+                i=i+3
+            else:
+                return nums[i]    
