119f9a1ca4c3c54be2aca0dde807dfb8b8bdd14f
ANURAG-R-NAIK <111006990+ANURAG-R-NAIK@users.noreply.github.com>
2023-01-07 11:16:00 +0530

    ..

---
+++ KUNAL_KUSHWA/L37.py
@@ -0,0 +1,21 @@
+import re
+# s='codestudio: a coding portal for coders'
+# match = re.search(r'portal', s)
+# print('start index', match.start())
+# print('end index :', match.end())
+
+######### TO FING=D THE DIGITS IN A STRING ###########
+
+# string = "hello my number is 123456677 and my friends number is 23880320990"
+# regex='\d+'
+# match = re.findall(regex, string)
+# print(match)
+
+##########  TO FIND A RANGE OF LETTERS FROM A STRING ############
+
+# p = re.compile('[a-e]')
+# print(p.findall("aye,said,mr.gandu stark"))
+
+
+
+re.split(pattern, string, maxsplit = 0, flag = 0)
