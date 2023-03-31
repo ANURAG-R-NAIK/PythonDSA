9849d9c757040dbcca86145a99327092ac1426c3
ANURAG-R-NAIK <111006990+ANURAG-R-NAIK@users.noreply.github.com>
2023-03-31 22:18:45 +0530

    31 march

---
+++ KUNAL_KUSHWA/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int x[10] = {0},h[10] = {0},y[20] = {0};
+    int i,j,n,k,l1,l2;
+    printf('enter the length of the first sequence');
+    scanf("%d",&l1);
+    printf('en')
+
+}
